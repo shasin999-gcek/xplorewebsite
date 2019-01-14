35f40ab7a52cfdd2fdf7de0eb089ecc69b5c92aa
akkiakshay <akkiakshay1997@gmail.com>
2019-01-15 03:14:08 +0530

    sponsor fixes

--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -35,58 +35,5 @@
           </div>
         </div>
       </div>
-      <section class="section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="card card-plain">
-              <div class="card-header">
-                <h1 class="profile-title text-left">Contact</h1>
-              </div>
-              <div class="card-body">
-                <form>
-                  <div class="row">
-                    <div class="col-md-6">
-                      <div class="form-group">
-                        <label>Your Name</label>
-                        <input type="text" class="form-control" value=" ">
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-group">
-                        <label>Email address</label>
-                        <input type="email" class="form-control" placeholder=" ">
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-6">
-                      <div class="form-group">
-                        <label>Phone</label>
-                        <input type="text" class="form-control" value=" ">
-                      </div>
-                    </div>
-                    <div class="col-md-6">
-                      <div class="form-group">
-                        <label>Subject</label>
-                        <input type="text" class="form-control" value=" ">
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label>Message</label>
-                        <input type="text" class="form-control" placeholder="Hello there!">
-                      </div>
-                    </div>
-                  </div>
-                  <button type="submit" class="btn btn-primary btn-round float-right" rel="tooltip" data-original-title="Can't wait for your message" data-placement="right">Send text</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>   
+        
 @endsection
