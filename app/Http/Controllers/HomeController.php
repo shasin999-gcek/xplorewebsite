dc4293a591a3aff022bb6759289ebf19bb35a65e
Muhammed Shasin P <mohdshasin313@gmail.com>
2019-01-16 11:50:53 +0530

    Minor bug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -104,6 +104,7 @@ class HomeController extends Controller
       $responseParamList = getTxnStatusNew($requestParamList);
       
       $responseParamList['CURRENCY'] = 'INR';
+      $responseParamList['GATEWAYNAME'] = 'UPI';
 
       unset($responseParamList['REFUNDAMT']);
 
