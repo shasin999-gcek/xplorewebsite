ecf0398fcf0de8700563fe3497df56ead7021923
akkiakshay <akkiakshay1997@gmail.com>
2019-01-23 22:18:15 +0530

    sponsor

--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -14,7 +14,11 @@
             <div class="col-md-10 ml-auto col-xl-2 mr-auto">              
               <img class="img-fluid floating" src="{{asset('img/bank.png')}}" width="300" height="300">
             </div>
-            
+          </div>
+          <div class="row">
+            <div class="col-md-12 ml-auto col-xl-12 mr-auto text-center">
+              <h4>Morazha-Kalliasseri Service Co-op Bank</h4>
+            </div>   
           </div>
           <div class="row">
             <div class="col-md-12 ml-auto col-xl-12 mr-auto">
@@ -25,12 +29,15 @@
           <div class="row row-grid justify-content-center">
             <div class="col-md-10 ml-auto col-xl-4 mr-auto text-center">
               <img class="img-fluid floating" src="{{ asset('img/icfoss.png') }}"/>
+              <h4 class="my-2">Event Partners</h4>
             </div>
             <div class="col-md-10 ml-auto col-xl-4 mr-auto text-center">
               <img class="img-fluid floating" src="{{ asset('img/eventoz.png') }}"/>
+              <h4 class="my-2">Promotional Partner</h4>
             </div>
             <div class="col-md-10 ml-auto col-xl-4 mr-auto text-center">
               <img class="img-fluid floating" src="{{ asset('img/buddha.png') }}"/>
+              <h4 class="my-2">Media Partner</h4>
             </div>
           </div>
         </div>
