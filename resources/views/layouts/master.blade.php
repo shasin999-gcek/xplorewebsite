45bf151ccefddf6eb664713d0cfc4ca170410ef0
Muhammed Shasin P <mohdshasin313@gmail.com>
2019-02-17 17:34:31 +0530

    Minor bug fixes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="theme-color" content="#171841">
   <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
   <link rel="manifest" href="/manifest.json">
